30415b8c944e71acda1fc7092b98c953294834c1
dominicom <dominik.kiepuszewski@gmail.com>
2022-11-13 17:47:01 +0100

    Deleted react-copy-to-clipboard package and replaced by navigator method, refactored <Glyph /> component

--- src/components/Glyph/Glyph.js
+++ src/components/Glyph/Glyph.js
@@ -1,40 +1,52 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { store } from '../../store.js';
 
-import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Icon from '../Icon';
 
 import './Glyph.scss';
+
+const unicodeToStr = (glyph) => {
+  return glyph.codePointAt(0).toString(16);
+};
+
+
 
 export const Glyph = (props) => {
   const { glyph, index } = props;
   const { state, dispatch } = useContext(store);
 
-  const copyToClipboard = (value) => {
-    console.log('copied value:', value, typeof value);
+  const [ symbol, setSymbol ] = useState(null);
 
+
+  const id = `glyph-${unicodeToStr(glyph)}`;
+
+  const copyToClipboard = (value) => {
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(value);
+      
+    // console.log('copied value:', value, typeof value);
 
     dispatch({ type: 'copied element', value: value });
     dispatch({ type: 'set copied', value: true });
-    console.log(state);
   };
-  const unicodeToStr = (glyph) => {
-    return glyph.codePointAt(0).toString(16);
-  };
+
+  useEffect(() => {
+    setSymbol(`${unicodeToStr(glyph)}`);
+  }, [])
+
   if (glyph) {
     return (
-      <CopyToClipboard text={glyph} onCopy={() => copyToClipboard(glyph)}>
-        <a id={`glyph-${unicodeToStr(glyph)}`} className="Glyph">
-          <div href={`#glyph-${unicodeToStr(glyph)}`}>
-            <span className="glyph-symbol responsive-heading-05">{glyph}</span>
-            {/* <div className="glyph-number">{index + 1}</div> */}
-            <div className="glyph-code">{unicodeToStr(glyph)}</div>
-            <div className="glyph-action">
-              <Icon type="Copy" stroke={1} />
-            </div>
+      <a id={id} className="Glyph" onClick={() => copyToClipboard(glyph)}>
+        <div href={`#${id}`}>
+          <span className="glyph-symbol responsive-heading-05">{glyph}</span>
+          {/* <div className="glyph-number">{index + 1}</div> */}
+          <div className="glyph-code">{symbol}</div>
+          <div className="glyph-action">
+            <Icon type="Copy" stroke={1} />
           </div>
-        </a>
-      </CopyToClipboard>
+        </div>
+      </a>
     );
   }
   return null;
